1939d9ca1c87e7eb70707640ff5fedcbb96d168f
pixelliquid <peter@pixelliquid.com>
2025-03-03 15:09:30 +0100

    follow along lesson 17

--- mario-forum/src/App.vue
+++ mario-forum/src/App.vue
@@ -1,5 +1,9 @@
 <script>
+import FruitStatistics from './components/FruitStatistics.vue';
 export default {
+  components: {
+    FruitStatistics,
+  },
   data: () => ({
     newCharacter: {
       name: '',
@@ -25,43 +29,6 @@ export default {
     ],
     favouritesList: [],
   }),
-  computed: {
-    // solution copilot:
-    favStatistics() {
-      const stats = {};
-      this.characterList.forEach(character => {
-        character.favs.forEach(fav => {
-          if (!stats[fav]) {
-            stats[fav] = { count:0, names:[]};
-          }
-          stats[fav].count++;
-          stats[fav].names.push(character.name);
-        });
-      });
-      return stats;
-    },
-    // solution Ben:
-    favStatisticsFixed() {
-      const favs = ['Avocado', 'Banana', 'Orange', 'Mango', 'Melon', 'Pineapple'];
-      const stats = {
-        Avocado: 0,
-        Banana: 0,
-        Orange: 0,
-        Mango: 0,
-        Melon: 0,
-        Pineapple: 0
-      };
-
-      this.characterList.forEach((character) => {
-        favs.forEach((fav) => {
-          if (character.favs.includes(fav)) {
-            stats[fav]++;
-          }
-        });
-      });
-      return stats;
-    }
-  },
   methods: {
     addToFavourites(character) {
       this.favouritesList.push(character);
@@ -113,22 +80,8 @@ export default {
     v-model="newCharacter.name"
     @keyup.enter="addCharacter" />
   <button @click="addCharacter" :disabled="!newCharacter.name">Add Character</button>
-  <h2>Statistics – fav fruits:</h2>
-  <pre>solution Ben (fixed array):</pre>
-  <!-- <pre>{{ favStatisticsFixed}}</pre> -->
-  <ul>
-    <!-- <li v-for="stat in favStatisticsFixed"> only shows value -->
-    <li v-for="(stat, fruit) in favStatisticsFixed" :key="`character-${stat}-${fruit}`">
-      {{ fruit }} is fav by {{ stat }}
-    </li>
-  </ul>
-  <pre>solution copilot (dynamic array):</pre>
-  <!-- <pre>{{ favStatistics}}</pre> -->
-  <ul>
-    <li v-for="(data, fruit) in favStatistics" :key="fruit">
-      {{ fruit }} is fav by {{ data.count }}: <span v-for="(name, index) in data.names" :key="name">{{ name }}{{ index === data.names.length - 1 ? '' : ', ' }}</span>
-    </li>
-  </ul>
+  
+  <FruitStatistics />
 </template>
 
 <style>
---
+++ mario-forum/src/components/FruitStatistics.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  data: () => ({
+  }),
+  computed: {
+    // solution copilot:
+    favStatistics() {
+      const stats = {};
+      this.characterList.forEach(character => {
+        character.favs.forEach(fav => {
+          if (!stats[fav]) {
+            stats[fav] = { count:0, names:[]};
+          }
+          stats[fav].count++;
+          stats[fav].names.push(character.name);
+        });
+      });
+      return stats;
+    },
+    // solution Ben:
+    favStatisticsFixed() {
+      const favs = ['Avocado', 'Banana', 'Orange', 'Mango', 'Melon', 'Pineapple'];
+      const stats = {
+        Avocado: 0,
+        Banana: 0,
+        Orange: 0,
+        Mango: 0,
+        Melon: 0,
+        Pineapple: 0
+      };
+
+      this.characterList.forEach((character) => {
+        favs.forEach((fav) => {
+          if (character.favs.includes(fav)) {
+            stats[fav]++;
+          }
+        });
+      });
+      return stats;
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>Statistics – fav fruits:</h2>
+  <pre>solution Ben (fixed array):</pre>
+  <!-- <pre>{{ favStatisticsFixed}}</pre> -->
+  <ul>
+    <!-- <li v-for="stat in favStatisticsFixed"> only shows value -->
+    <li v-for="(stat, fruit) in favStatisticsFixed" :key="`character-${stat}-${fruit}`">
+      {{ fruit }} is fav by {{ stat }}
+    </li>
+  </ul>
+  <pre>solution copilot (dynamic array):</pre>
+  <!-- <pre>{{ favStatistics}}</pre> -->
+  <ul>
+    <li v-for="(data, fruit) in favStatistics" :key="fruit">
+      {{ fruit }} is fav by {{ data.count }}: <span v-for="(name, index) in data.names" :key="name">{{ name }}{{ index === data.names.length - 1 ? '' : ', ' }}</span>
+    </li>
+  </ul>
+</template>
